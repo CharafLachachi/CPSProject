83f07aea53e1a6cc7f4a4072b255389f2fda8c7f
3602541 <3602541@ppti-14-509-05.ufr-info-p6.jussieu.fr>
2018-04-10 18:36:01 +0200

    Service Mob et ses sous services

---
+++ tme7/src/services/Player.java
@@ -0,0 +1,65 @@
+package services;
+
+import java.util.Optional;
+
+public interface Player extends /* include */ EntityService {
+
+	// observators
+
+	public Optional<Command> lastCom();
+
+	/**
+	 * @param x
+	 * @param y
+	 * @return pre -1 <= x <= 1 and -1 <= y <= 3
+	 */
+	public Optional<MobService> content(int x, int y);
+
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @return pre -1 <= x <= 1 and -1 <= y <= 3
+	 */
+
+	public Cell nature(int x, int y);
+
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @return pre -1 <= x <= 1 and -1 <= y <= 3
+	 */
+
+	public Cell viewable(int x, int y);
+
+	/**
+	 * post LastCom(P)=FF implies step(P) = Forward(P)
+	 * post LastCom(P)=BB implies step(P) = Backward(P) 
+	 * post LastCom(P)=LL implies step(P) = StrafeLeft(P)
+	 * post LastCom(P)=RR implies step(P) = StrafeRight(P) 
+	 * post LastCom(P)=TL implies step(P) = TurnLeft(P)
+	 * post LastCom(P)=TR implies step(P) = TurnRight(P)
+	 */
+
+	public void step();
+	
+/**
+ * [Invariant]
+ * 	        getFace() = N implies content(u,v) = getEnv().CellContent(Envi(P),getCol()+u,getRow()+v)
+ *			getFace() = N implies nature(u,v) = getEnv().getCellNature(getCol()+u,getRow()+v)
+ *			getFace() = S implies content(u,v) = getEnv().CellContent(Envi(P),getCol()-u,getRow()-v)
+ *			getFace() = S implies nature(u,v) = getEnv().getCellNature(getCol()-u,getRow()-v)
+ *			getFace() = E implies content(u,v) = getEnv().CellContent(Envi(P),getCol()+v,getRow()-u)
+ *			getFace() = E implies nature(u,v) = getEnv().getCellNature(getCol()+v,getRow()-u)
+ *			getFace() = W implies content(u,v) = getEnv().CellContent(Envi(P),getCol()-v,getRow()+u)
+ *			getFace() = W implies nature(u,v) = getEnv().getCellNature(getCol()-v,getRow()+u)
+ *			forall u,v in [-1,1] * [-1,1], not viewable(u,v)
+ *			viewable(,-1,2) = nature(-1,1) in {WALL, DWC, DNC}
+ *			viewable(,0,2) = nature(0,1) in {WALL, DWC, DNC}
+ *			viewable(,1,2) = nature(1,1) in {WALL, DWC, DNC}
+ * 			viewable(,-1,3) = nature(-1,2) in {WALL, DWC, DNC} and viewable(-1,2)
+ *			viewable(,0,3) = nature(0,2) in {WALL, DWC, DNC} and viewable(0,2)
+ *			viewable(,1,3) = nature(1,2) in {WALL, DWC, DNC} and viewable(1,2)
+	*/
+}
